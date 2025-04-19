12032ce089cb565a29d0dcbb88d07e913283a123
med amine nassim <m.nassim152@gmail.com>
2025-04-19 17:59:22 +0100

    who likes it ?

---
+++ 6kyu/Wholikesit.py
@@ -0,0 +1,16 @@
+# You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+# Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+def likes(names):
+    match len(names):
+        case 0:
+            return "no one likes this"
+        case 1:
+            return f'{names[0]} likes this'
+        case 2:
+            return f'{names[0]} and {names[1]} like this'
+        case 3:
+            return f'{names[0]}, {names[1]} and {names[2]} like this'
+        case default:
+            return f'{names[0]}, {names[1]} and {len(names[1:-1])} others like this'
