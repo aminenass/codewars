51a35be2f1e5d44d99d46725b9cbd019448f43e6
med amine nassim <m.nassim152@gmail.com>
2025-05-03 23:28:00 +0100

    Convert a string to an array

---
+++ 7kyu/Convert_a_string_array.go
@@ -0,0 +1,15 @@
+// Description:
+// Write a function to split a string and convert it into an array of words.
+
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+package kata
+import "strings"
+
+func StringToArray(str string) []string {
+  // your code here
+	return strings.Split(str," ")
+}
